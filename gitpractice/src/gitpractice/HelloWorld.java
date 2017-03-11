53a40af4b2392c43396f1668c1048515d7ce1f74
SeetharamanGit <Seetharaman.sakthi@gmail.com>
2017-03-10 19:50:42 -0500

    Update HelloWorld.java
    Added for pull request from git hub

--- gitpractice/src/gitpractice/HelloWorld.java
+++ gitpractice/src/gitpractice/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World");
-		System.out.println("hello world1");
+		System.out.println("Seetharaman Pull request");
 		
 		System.out.println("Added");
 		System.out.println("Branch1");
